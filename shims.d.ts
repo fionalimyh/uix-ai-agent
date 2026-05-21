edcf9d3b68c9e0bd34450ae2535ae4a16dad5b65
fionalyh <fionalimyh@gmail.com>
2026-05-21 23:07:31 +0800

    Update

---
+++ shims.d.ts
@@ -0,0 +1,28 @@
+declare module "reactflow" {
+  const ReactFlow: any;
+  export default ReactFlow;
+  export const Background: any;
+  export const useNodesState: any;
+  export const useEdgesState: any;
+  export const Panel: any;
+  export const ReactFlowProvider: any;
+  export const useReactFlow: any;
+  export const NodeToolbar: any;
+  export const Position: any;
+  export const BackgroundVariant: any;
+  export type NodeProps<T = any> = T;
+}
+
+declare module "jszip" {
+  const JSZip: any;
+  export default JSZip;
+}
+
+declare module "marked" {
+  export const marked: any;
+}
+
+declare module "@google/genai" {
+  export const GoogleGenAI: any;
+  export const Type: any;
+}
